81742135c7fd363e23e5eb07335e6e6e518173ab
LAPTOP-Cho_Daeyeong <jdy0818@gmail.com>
2023-09-22 11:21:37 +0900

    그래프 그리는 스레드 구현

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import os
 import sys
+import threading
 import numpy as np
 import time
-import threading
 import random
 
 from PyQt5.QtWidgets import *
@@ -20,12 +20,14 @@ matplotlib.use('QT5Agg')
 
 from define import *
 
+# 프로그램 아이콘 생성
 def resource_path(relative_path):
     base_path = getattr(sys, "_MEIPASS", os.path.dirname(os.path.abspath(__file__)))    
     return os.path.join(base_path, relative_path)
-
-form = resource_path('main.ui')
+form = resource_path(ICON_FILE_NAME)
 form_class = uic.loadUiType(form)[0]
+
+
 
 class MixArray(threading.Thread):
     def __init__(self, array, mixNum):
@@ -34,46 +36,91 @@ class MixArray(threading.Thread):
         self.mixNum = mixNum
 
     def run(self):
-        while True:
+        for _ in range(self.mixNum):
+            print("shuffle array")
             sampleList = random.sample(list(range(2, 70)), 2)
             self.array[sampleList[0]], self.array[sampleList[1]] = self.array[sampleList[1]], self.array[sampleList[0]]
+            time.sleep(1/60)
 
 
+
+## 그래프 표현 클래스 ##
+class ViewGraph(QThread):
+    def __init__(self, array, pivot, compare, fixed):
+        super().__init__()
+        self.array = array
+        self.pivot = pivot
+        self.compare = compare
+        self.fixed = fixed
+        self.fig, self.ax = plt.subplots()
+        self.canvas = FigureCanvas(self.fig)
+        self.ax = plt.bar(np.arange(1, len(self.array) + 1), self.array, width=0.5)
+        self.canvas.draw()
+
+    def run(self):
+        while True:
+            print("draw graph")
+            for height, barRectangle in zip(self.array, self.ax):
+                barRectangle.set_height(height)
+                if height in self.pivot:
+                    barRectangle.set_color("C1")
+                elif height in self.compare:
+                    barRectangle.set_color("C2")
+                elif height in self.fixed:
+                    barRectangle.set_color("C3")
+                else:
+                    barRectangle.set_color("C0")
+            self.canvas.draw()
+            time.sleep(1/60)
+        self.canvas.close()
+
+
+
+# 메인 윈도우
 class WindowClass(QMainWindow, form_class):
     def __init__(self):
         super( ).__init__( )
         ## ========== 정의 ========== ##
 
         # 박스 위젯 정의
-        self.tabWidgetControl = QTabWidget()
-        self.frameGraph = QFrame()
-        self.verticalLayoutGraph = QVBoxLayout()
-        self.listWidgetState = QListWidget()
-        self.listWidgetLog = QListWidget()
+        self.tabWidgetControl: QTabWidget
+        self.frameGraph: QFrame
+        self.verticalLayoutGraph: QVBoxLayout
+        self.listWidgetState: QListWidget
+        self.listWidgetLog: QListWidget
         
-        self.testButton1 = QPushButton()
-        self.testButton2 = QPushButton()
-        self.testButton3 = QPushButton()
-        self.testButton4 = QPushButton()
-        self.testButton5 = QPushButton()
-        self.testButton6 = QPushButton()
-
+        self.testButton1: QPushButton
+        self.testButton2: QPushButton
+        self.testButton3: QPushButton
+        self.testButton4: QPushButton
+        self.testButton5: QPushButton
+        self.testButton6: QPushButton
         ## 초기 세팅 ##
-        
-        self.array = list(range(1, 101))
 
-        print(self.array)
-        
         self.setupUi(self) # ui 임포트
-        
-        self.add_graph_to_layout() # 그래프 삽입
 
-        t = MixArray(self.array, 100)
-        t.start()
+        self.array = list(range(1, 101))
+        self.pivot = [1, 4, 6]
+        self.compare = [5, 7 ,9]
+        self.fixed = [10, 11, 12]
+
+        ## 그래프 출력 및 애니메이션 실행 ##
+        self.canvas: FigureCanvas
+        self.animationThread = ViewGraph(self.array, self.pivot, self.compare, self.fixed)
+        self.verticalLayoutGraph.addWidget(self.animationThread.canvas)
+
+        self.animationThread.start()
+
+
+        self.mix = MixArray(self.array, 100)
+        self.mix.daemon = True
+        self.mix.start()
+        
+
         
         ## ================================================= 시그널 들 ================================================= ##
     
-
+    
 
 
     
@@ -85,49 +132,17 @@ class WindowClass(QMainWindow, form_class):
         self.testButton4.clicked.connect(self.testFunc4)
         self.testButton5.clicked.connect(self.testFunc5)
         self.testButton6.clicked.connect(self.testFunc6)
-        # x = np.arange(0, 5)
-        # self.ax.bar(x, [1, 5, 3, 4, 2])
 
         # 여기에 시그널, 설정 
     ## ================================================= 함수 들 ================================================= ##
-
-    # 그래프 생성 함수
-    def add_graph_to_layout(self):
-        # Figure, FigureCanvas 생성
-        self.fig, self.ax = plt.subplots()
-        self.canvas = FigureCanvas(self.fig)
-
-        # FigureCanvas를 QVBoxLayout에 추가
-        self.verticalLayoutGraph.addWidget(self.canvas)
-        self.animation = FuncAnimation(self.fig, self.update, frames=self.array, init_func=self.init_bars, blit=True, interval=1)
-        self.canvas.draw()
-
-        del(self.animation)
-
-
-    def init_bars(self):
-        x = np.arange(len(self.array))
-        self.bars = self.ax.bar(x, self.array, color='C0')
-        return self.bars
-
-    def update(self, frame):
-        x = np.arange(len(self.array))
-        self.bars = self.ax.bar(x, self.array, color='C0')
-        # self.bars = self.ax.bar(x, self.array, color='blue')
-        # self.bars[1].set_facecolor('red')
-        return self.bars
-    
-
 
 
 
     ## 테스트 시그널 함수 ##
     def testFunc1(self):
-        self.animation.pause()
         print("test1 message")
 
     def testFunc2(self):
-        self.animation.resume()
         print("test2 message")
 
     def testFunc3(self):
--- test2.py
+++
@@ -1,9 +0,0 @@
-import matplotlib.pyplot as plt
-import numpy as np
-
-
-
-plt.bar([1, 5, 3], [1, 2 ,3], label="Price ($)", color="C1")
-plt.legend()
-
-plt.show()
