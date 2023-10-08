9bb091e01c4b1f706ffa37bd4f2b188e38bfddcc
PC-Cho_Daeyeong <jdy0818@gmail.com>
2023-10-09 02:01:17 +0900

    로그 기능 추가, sql 추가

--- algorithms.py
+++ algorithms.py
@@ -1,25 +1,23 @@
 import time
 
-class SortAlgorithms():
+class MyAlgorithms():
     def __init__(self):
         self.sort_algorithms = {
-            "버블 정렬":self.bubble_sort(),
-            "선택 정렬":self.selection_sort()
+            "버블 정렬":self.bubble_sort,
+            "선택 정렬":self.selection_sort,
         }
         self.search_algorithms = {
-            
+            "선형 탐색":self.linear_search,
         }
-        self.algorithms_name = ["버블 정렬", "선택 정렬"]
-        self.algorithms_func = [self.bubble_sort(), self.selection_sort()]
 
 ## ========================================================================== 정렬 알고리즘 ========================================================================== ##
+# 사용 가능한 전역 변수: array, pivot, compare, other_compare, compare_list, fix
+
 
     def bubble_sort(self): # 버블 정렬
         global array
         global pivot
         global compare
-        global compare_other
-        global compare_list
         global fix
 
         for i in range(len(array) - 1, 0, -1):
@@ -29,6 +27,7 @@ class SortAlgorithms():
                 if array[pivot] > array[compare]:
                     array[pivot], array[compare] = array[compare], array[pivot]
             self.fixbar(i)
+
 
     def selection_sort(self): # 선택 정렬
         global array
@@ -47,7 +46,38 @@ class SortAlgorithms():
             array[i], array[pivot] = array[pivot], array[i]
             self.fixbar(i)
 
+
 ## ========================================================================== 정렬 알고리즘 끝 ========================================================================== ##
+
+## ========================================================================== 탐색 알고리즘 ========================================================================== ##
+
+
+    def linear_search(self):
+        global array
+        global pivot
+        global search_value
+
+        for pivot in array:
+            self.delay()
+            if pivot == search_value:
+                return pivot
+
+
+
+## ========================================================================== 탐색 알고리즘 끝 ========================================================================== ##
+
+
+
+
+
+    def runSortFunc(self, name):
+        self.sort_algorithms[name]()
+
+    def runSearchFunc(self, name):
+        global fix
+        self.search_value = self.search_algorithms[name]()
+        fix.append(self.search_value)
+        self.delay()
 
     def delay(self): # 딜레이 넣기
         time.sleep(limit/1000)
@@ -62,5 +92,6 @@ compare: int = -1
 compare_other: int = -1
 compare_list: list = []
 fix: list = []
+search_value: int
 
 limit: int
--- test.py
+++ test.py
@@ -5,7 +5,6 @@ import shutil
 import sys
 import time
 import random
-import sqlite3
 
 #PyQt5 임포트
 from PyQt5.QtWidgets import *
@@ -156,16 +155,11 @@ class ViewGraph(QObject):
 
 ## ================================================= 알고리즘 실행 스레드 클래스 ================================================= ##
 class AlgorithmSimulation(QThread):
-    start_signal = pyqtSignal()
-    sort_finished_signal = pyqtSignal()
-    search_finished_signal = pyqtSignal()
+    finished_signal = pyqtSignal()
 
     def __init__(self):
         super().__init__()
-        self.simulation = gb.MyAlgorithms()
-
-        self.sort_algorithms_list = list(self.simulation.sort_algorithms.keys())
-        self.search_algorithms_list = list(self.simulation.search_algorithms.keys())
+        self.sort_simulation = gb.SortAlgorithms()
 
     def run(self):
         global input_algorithm
@@ -173,11 +167,6 @@ class AlgorithmSimulation(QThread):
         global input_speed
         global input_shuffle
         global input_shuffle_check
-        global input_issort
-        global input_search_value
-        global input_isuser_value
-        global input_repeat_value
-        global input_isrepeat
 
         gb.pivot = -1
         gb.compare = -1
@@ -185,39 +174,19 @@ class AlgorithmSimulation(QThread):
         gb.compare_list = []
         gb.fix = []
 
-        if input_issort:
-            if input_shuffle_check:
-                self.shuffleLimitFunc()
-            else:
-                self.shuffleFunc()
-
-            self.start_signal.emit()
-
-            self.simulation.runSortFunc(input_algorithm)
-
-            self.isSort()
-
-            self.sort_finished_signal.emit()
+        if input_shuffle_check:
+            self.shuffleLimitFunc()
         else:
-            self.start_signal.emit()
+            self.shuffleFunc()
 
-            self.simulation.runSearchFunc(input_algorithm)
+        if input_algorithm == "버블 정렬":
+            self.sort_simulation.bubble_sort()
+        elif input_algorithm == "선택 정렬":
+            self.sort_simulation.selection_sort()
 
-            if input_isrepeat:
-                for _ in range(1, input_repeat_value):
-                    gb.pivot = -1
-                    gb.compare = -1
-                    gb.compare_other = -1
-                    gb.compare_list = []
-                    gb.fix = []
+        self.isSort()
 
-                    if not input_isuser_value:
-                        gb.search_value = random.randrange(input_size)
-
-                    self.simulation.runSearchFunc(input_algorithm)
-
-
-            self.search_finished_signal.emit()
+        self.finished_signal.emit()
 
     def shuffleFunc(self): # 지연 없이 섞기
         for _ in range(input_shuffle):
@@ -260,13 +229,6 @@ class WindowClass(QMainWindow, form_class):
         self.setWindowIcon(QIcon(resource_path(ICON_PATH))) # 아이콘 임포트
 
         ## ==================== 정의 ==================== ##
-        # 전역 변수
-        global input_algorithm
-        global input_size
-        global input_speed
-        global input_shuffle
-        global input_shuffle_check
-        global input_issort
 
         # 박스 위젯 정의
         self.tabWidgetControl: QTabWidget
@@ -284,19 +246,7 @@ class WindowClass(QMainWindow, form_class):
         self.pushButton_sort_start: QPushButton
         self.pushButton_sort_restart: QPushButton
         self.pushButton_sort_stop: QPushButton
-
-        # 탐색 알고리즘 위젯
-        self.comboBox_search_select_algorithm: QComboBox
-        self.spinBox_search_data_size: QSpinBox
-        self.spinBox_search_speed_limit: QSpinBox
-        self.spinBox_search_search_number: QSpinBox
-        self.checkBox_search_set_search_number: QCheckBox
-        self.spinBox_search_repeat_number: QSpinBox
-        self.checkbox_search_set_repeat_number: QCheckBox
-        self.pushButton_search_start: QPushButton
-        self.pushButton_search_restart: QPushButton
-        self.pushButton_search_stop:QPushButton
-
+        
         # 테스트 버튼 위젯
         self.testButton1: QPushButton
         self.testButton2: QPushButton
@@ -307,34 +257,14 @@ class WindowClass(QMainWindow, form_class):
         self.viewGraph = ViewGraph() # 그래프 표현 클래스 객체 생성
         self.verticalLayoutGraph.addWidget(self.viewGraph.graph) # verticalLayoutGraph에 viewGraph 올림
 
-        self.runTime = RunTimer() # 타이머 객체 생성
-        self.runTime.run_time_signal.connect(self.timer_worked) # 타이머 시그널
-
-        self.algorithmSimulation = AlgorithmSimulation() # 알고리즘 시뮬레이션 객체 생성
-        self.algorithmSimulation.start_signal.connect(self.start_algorithm_thread)
-        self.algorithmSimulation.sort_finished_signal.connect(self.sort_thread_finished)
-        self.algorithmSimulation.search_finished_signal.connect(self.search_thread_finished)
-
-        self.tabWidgetControl.currentChanged.connect(self.tabChangeSignal)
-
-        self.comboBox_sort_select_Algorithm.addItems(self.algorithmSimulation.sort_algorithms_list) # 알고리즘 종류를 불러와 콤보박스에 입력
+        self.runTime = RunTimer()
+        self.runTime.run_time_signal.connect(self.timer_worked)
         
-        self.comboBox_search_select_algorithm.addItems(self.algorithmSimulation.search_algorithms_list) # 알고리즘 종류를 불러와 콤보박스에 입력
 
-        input_algorithm = self.comboBox_sort_select_Algorithm.currentText()
-        input_size = self.spinBox_sort_data_size.value()
-        input_speed = self.spinBox_sort_speed_limit.value()
-        input_shuffle = self.spinBox_sort_shuffle_number.value()
-        input_shuffle_check = self.checkBox_sort_shuffle_number.isChecked()
-        input_issort = True
+        self.algorithmSimulation = AlgorithmSimulation()
 
         self.stateListItem = self.listWidgetState.count() # designer에서 listWidget(State)에 입력해 놓은 기존 값 도출
-        self.state_item: QListWidget.item() = None
-        self.timer_item: QListWidget.item() = None
-
-        self.listWidgetLog.clear()
-
-        self.tabChangeSignal(0)
+        self.items = [self.listWidgetState.item(i) for i in range(self.stateListItem)] # self.items[n] 0: 상태, 1: 진행 시간, 2: 알고리즘 명, 3: 데이터 크기, 4: 속도 제한, 5: 섞는 횟수, 6: 탐색 값
 
         ## ==================== 코드 ==================== ##
 
@@ -349,15 +279,6 @@ class WindowClass(QMainWindow, form_class):
         self.pushButton_sort_restart.clicked.connect(self.sortRestartFunc)
         self.pushButton_sort_stop.clicked.connect(self.sortStopFunc)
 
-
-        # 탐색 알고리즘 시그널
-        self.pushButton_search_start.clicked.connect(self.searchStartFunc)
-        self.pushButton_search_restart.clicked.connect(self.searchRestartFunc)
-        self.pushButton_search_stop.clicked.connect(self.searchStopFunc)
-
-
-
-
         self.testButton1.clicked.connect(self.testFunc1)
         self.testButton2.clicked.connect(self.testFunc2)
         self.testButton3.clicked.connect(self.testFunc3)
@@ -365,24 +286,21 @@ class WindowClass(QMainWindow, form_class):
     ## ==================== 함수 ==================== ##
 
     
-    ## 정렬 알고리즘 함수 ##
+    # 정렬 알고리즘 함수
     def sortStartFunc(self): # 실행 버튼 클릭 시 동작
         global input_algorithm
         global input_size
         global input_speed
         global input_shuffle
         global input_shuffle_check
-        global input_issort
-
-        input_issort = True
         
         self.pushButton_sort_start.setEnabled(False)
         self.pushButton_sort_restart.setEnabled(True)
         self.pushButton_sort_stop.setEnabled(True)
-        self.tabWidgetControl.setTabEnabled(1, False)
 
-        self.getSortWidgetValue()
-        
+        self.getWidgetValue()
+        self.runTime.newStart()
+        self.statesSetFunc(self.input_algorithm, self.runTime.getRunTime(), self.input_algorithm, self.input_size, self.input_speed, self.input_shuffle)
         self.sortFunc()
 
 
@@ -397,7 +315,8 @@ class WindowClass(QMainWindow, form_class):
         self.viewGraph.stop()
         self.runTime.stop()
 
-        gb.array = list(range(input_size))
+        self.runTime.newStart()
+        self.statesSetFunc(self.input_algorithm, self.runTime.getRunTime(), self.input_algorithm, self.input_size, self.input_speed, self.input_shuffle)
 
         self.sortFunc()
 
@@ -412,13 +331,12 @@ class WindowClass(QMainWindow, form_class):
 
         self.pushButton_sort_start.setEnabled(True)
         self.pushButton_sort_stop.setEnabled(False)
-        self.tabWidgetControl.setTabEnabled(1, True)
 
 
 
 
 
-    def getSortWidgetValue(self): # 정렬하기 위해 입력한 값들 추출
+    def getWidgetValue(self): # 정렬하기 위해 입력한 값들 추출
         global input_algorithm
         global input_size
         global input_speed
@@ -434,268 +352,87 @@ class WindowClass(QMainWindow, form_class):
         gb.limit = input_speed
         gb.array = list(range(input_size))
 
-        self.statesInitFunc({
-            "상태":"섞는 중",
-            "진행 시간":"00:00.000",
-            "알고리즘 명":input_algorithm,
-            "데이터 크기":input_size,
-            "속도 제한":input_speed,
-            "섞는 횟수":input_shuffle
-        })
-
 
 
 
 
     def sortFunc(self): # AlgorithmSimulation 클래스 호출하여 정렬
+        self.algorithmSimulation = AlgorithmSimulation(input_algorithm, input_speed, self.input_shuffle, self.input_shuffle_check)
+        self.algorithmSimulation.finished_signal.connect(self.sort_thread_finished)
+
         self.viewGraph.start()
-        self.algorithmSimulation.start()
+        self.algorithmSimulation.start(self.input_speed)
 
 
 
 
 
     def sort_thread_finished(self): # AlgorithmSimulation 클래스에서 정렬 종료 시 자동 호출
+        self.runTime.stop()
         self.sortStopFunc()
         self.viewGraph.draw_graph()
-        self.state_item.setText(f"상태: 정렬 완료")
-        self.sort_insert_db()
-
-
 
     
 
-    def sort_insert_db(self):
-        global input_algorithm
-        global input_size
-        global input_speed
-        global input_shuffle
 
-        connection = sqlite3.connect("algorithm_log.db")
-        cursor = connection.cursor()
 
-        cursor.execute("INSERT INTO sort_algorithm (name, time, size, speed, shuffle) VALUES (?, ?, ?, ?, ?)", (input_algorithm, self.runTime.getRunTime(), input_size, input_speed, input_shuffle))
-        connection.commit()
-
-        connection.close()
-
-        self.tabChangeSignal(0)
+    # 타이머
+    def timer_worked(self):
+        self.stateRunTimeSetFunc(self.runTime.getRunTime())
 
 
 
 
+    # State 화면 조작
+    def statesSetFunc(self, algorithm_state: str = "", run_time: str = "00:00.000", algorithm_name: str = "", data_size: int = -1, speed_limite: int = -1, shuffle_number: int = -1, search_value: int = -1): # self.items[n] 0: 상태, 1: 진행 시간, 2: 알고리즘 명, 3: 데이터 크기, 4: 속도 제한, 5: 섞는 횟수, 6: 탐색 값
+        print(1)
+        self.items[0].setText(f"상태: {algorithm_state}")
+        self.items[1].setText(f"진행 시간: {run_time}")
+        self.items[2].setText(f"알고리즘 명: {algorithm_name}")
+        self.items[3].setText(f"데이터 크기: {str(data_size)}")
+        self.items[4].setText(f"속도 제한: {str(speed_limite)}")
+        self.items[5].setText(f"섞는 횟수: {str(shuffle_number)}")
+        self.items[6].setText(f"탐색 값: {str(search_value)}")
 
-    ## 탐색 알고리즘 함수 ##
-    def searchStartFunc(self):
-        global input_algorithm
-        global input_size
-        global input_speed
-        global input_shuffle
-        global input_shuffle_check
-        global input_issort
-
-        input_issort = False
-        
-        self.pushButton_search_start.setEnabled(False)
-        self.pushButton_search_restart.setEnabled(True)
-        self.pushButton_search_stop.setEnabled(True)
-        self.tabWidgetControl.setTabEnabled(0, False)
-
-        self.getSearchWidgetValue()
-        self.searchFunc()
-
-
-
-
-
-    def searchRestartFunc(self):
-        self.pushButton_search_start.setEnabled(False)
-        self.pushButton_search_stop.setEnabled(True)
-
-        self.algorithmSimulation.terminate()
-        self.viewGraph.stop()
-        self.runTime.stop()
-
-        self.sortFunc()
-
-
-
-
-
-    def searchStopFunc(self):
-        self.algorithmSimulation.terminate()
-        self.viewGraph.stop()
-        self.runTime.stop()
-
-        self.pushButton_search_start.setEnabled(True)
-        self.pushButton_search_stop.setEnabled(False)
-        self.tabWidgetControl.setTabEnabled(0, True)
-
-
-
-
-
-    def getSearchWidgetValue(self):
-        global input_algorithm
-        global input_size
-        global input_speed
-        global input_search_value
-        global input_isuser_value
-        global input_repeat_value
-        global input_isrepeat
-
-        input_algorithm = self.comboBox_search_select_algorithm.currentText()
-        input_size = self.spinBox_search_data_size.value()
-        input_speed = self.spinBox_search_speed_limit.value()
-        input_isuser_value = self.checkBox_search_set_search_number.isChecked()
-        if input_isuser_value:
-            input_search_value = self.spinBox_search_search_number.value()
-        else:
-            input_search_value = random.randrange(input_size)
-        input_isrepeat = self.checkbox_search_set_repeat_number.isChecked()
-        if input_isrepeat:
-            input_repeat_value = self.spinBox_search_repeat_number.value()
-        else:
-            input_repeat_value = 1
-
-        gb.limit = input_speed
-        gb.array = list(range(input_size))
-        gb.search_value = input_search_value
-
-        self.statesInitFunc({
-            "상태":"탐색 중",
-            "진행 시간":"00:00.000",
-            "알고리즘 명":input_algorithm,
-            "데이터 크기":input_size,
-            "속도 제한":input_speed,
-            "탐색 값":input_search_value,
-            "반복 횟수":input_repeat_value
-        })
-
-
-
-
-    
-    def searchFunc(self): # AlgorithmSimulation 클래스 호출하여 정렬
-        self.viewGraph.start()
-        self.algorithmSimulation.start()
-
-
-
-
-
-    def search_thread_finished(self):
-        self.searchStopFunc()
-        self.viewGraph.draw_graph()
-        self.state_item.setText("상태: 탐색 완료")
-        self.search_insert_db()
-
-
-
-
-    def search_insert_db(self):
-        global input_algorithm
-        global input_size
-        global input_speed
-        global input_search_value
-        global input_repeat_value
-
-        connection = sqlite3.connect("algorithm_log.db")
-        cursor = connection.cursor()
-
-        cursor.execute("INSERT INTO search_algorithm (name, time, size, speed, value, repeat) VALUES (?, ?, ?, ?, ?, ?)", (input_algorithm, self.runTime.getRunTime(), input_size, input_speed, input_search_value, input_repeat_value))
-        connection.commit()
-
-        connection.close()
-
-        self.tabChangeSignal(1)
-
-
-
-
-
-    ## 공동 알고리즘 함수 ##
-    def statesInitFunc(self, states_dictionary: dict):
-        self.listWidgetState.clear()
-
-        self.key_list = list(states_dictionary.keys())
-        self.value_list = list(states_dictionary.values())
-
-        for key, value in zip(self.key_list, self.value_list):
-            self.listWidgetState.addItem(key + ": " + str(value))
-
-        self.state_item = self.listWidgetState.item(0)
-        self.timer_item = self.listWidgetState.item(1)
-
-    def tabChangeSignal(self, index):
-        self.listWidgetLog.clear()
-
-        connection = sqlite3.connect("algorithm_log.db")
-        cursor = connection.cursor()
-
+    def stateSetFunc(self, index: str, value): # self.items[n] 0: 상태, 1: 진행 시간, 2: 알고리즘 명, 3: 데이터 크기, 4: 속도 제한, 5: 섞는 횟수, 6: 탐색 값
         if index == 0:
-            cursor.execute("SELECT * FROM sort_algorithm")
-
-            for row in cursor.fetchall():
-                self.listWidgetLog.addItem(str(row[0]) + ", " + str(row[1]) + ", " + str(row[2]))
-
+            self.items[0].setText(f"상태: {value}")
         elif index == 1:
-            cursor.execute("SELECT * FROM search_algorithm")
+            self.items[1].setText(f"진행 시간: {value}")
+        elif index == 2:
+            self.items[2].setText(f"알고리즘 명: {value}")
+        elif index == 3:
+            self.items[3].setText(f"데이터 크기: {str(value)}")
+        elif index == 4:
+            self.items[4].setText(f"속도 제한: {str(value)}")
+        elif index == 5:
+            self.items[5].setText(f"섞는 횟수: {str(value)}")
+        elif index == 6:
+            self.items[6].setText(f"탐색 값: {str(value)}")
 
-            for row in cursor.fetchall():
-                self.listWidgetLog.addItem(str(row[0]) + ", " + str(row[1]) + ", " + str(row[2]))
-
-        connection.close()
-
-
-
-
-
-    def start_algorithm_thread(self):
-        self.runTime.newStart()
-
-
-
+    def stateRunTimeSetFunc(self, time):
+        self.items[1].setText(f"진행 시간: {time}")
 
 
-    ## 프로그램 종료 시 작동 함수 ##
+
+
+
+    # 프로그램 종료 시 작동 함수
     def closeEvent(self, event):
         event.accept()
 
 
 
-    ## 타이머 ##
-    def timer_worked(self):
-        self.stateRunTimeSetFunc(self.runTime.getRunTime())
-
-    def stateRunTimeSetFunc(self, time):
-        self.timer_item.setText(f"진행 시간: {time}")
 
 
 
 
-    ## 테스트 버튼 함수 ##
     def testFunc1(self):
-        self.tabChangeSignal(0)
+        self.runTime.start()
         print("test1")
 
     def testFunc2(self):
-        connection = sqlite3.connect("algorithm_log.db")
-
-        # 커서 객체 생성
-        cursor = connection.cursor()
-
-        # 모든 데이터를 조회하는 SQL 쿼리 실행
-        cursor.execute("SELECT * FROM sort_algorithm")
-
-        # fetchall()을 사용하여 모든 행을 가져오고, 반복문을 사용하여 출력
-        for row in cursor.fetchall():
-            print(row)
-
-        # 커서와 연결 종료 (옵션, but recommended)
-        cursor.close()
-        connection.close()
-        
+        print(self.runTime.getRunTime())
         print("test2")
 
     def testFunc3(self):
@@ -711,11 +448,6 @@ input_size: int
 input_speed: int
 input_shuffle: int
 input_shuffle_check: bool
-input_issort: bool
-input_search_value: int
-input_isuser_value: bool
-input_repeat_value: int
-input_isrepeat: bool
 
 fps: int
 
@@ -736,42 +468,7 @@ file.close()
 
 fps = bind_value[0]
 
-# DB 처리
-connection = sqlite3.connect("algorithm_log.db")
-cursor = connection.cursor()
 
-table_name = "sort_algorithm"
-cursor.execute(f"SELECT count(name) FROM sqlite_master WHERE type='table' AND name='{table_name}'")
-
-if not (cursor.fetchone()[0] == 1):
-    cursor.execute('''
-    CREATE TABLE sort_algorithm (
-        id INTEGER PRIMARY KEY,
-        name CHAR,
-        time CHAR,
-        size INTEGER,
-        speed INTEGER,
-        shuffle INTEGER
-    )
-    ''')
-
-table_name = "search_algorithm"
-cursor.execute(f"SELECT count(name) FROM sqlite_master WHERE type='table' AND name='{table_name}'")
-
-if not (cursor.fetchone()[0] == 1):
-    cursor.execute('''
-    CREATE TABLE search_algorithm (
-        id INTEGER PRIMARY KEY,
-        name CHAR,
-        time CHAR,
-        size INTEGER,
-        speed INTEGER,
-        value INTEGER,
-        repeat INTEGER
-    )
-    ''')
-
-connection.close()
 
 
 # 메인 함수
